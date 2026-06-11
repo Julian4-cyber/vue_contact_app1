38186b5d82996951bc131a167ffb057a2bbc96c5
Julian4-cyber <julian.pherionix@gmail.com>
2026-06-10 22:49:13 -0500

    ninth commit

--- src/components/aboutme.vue
+++ src/components/aboutme.vue
@@ -3,20 +3,22 @@
     <div class="col-md-4 text-center mb-4 mb-md-0">
       <img src="../assets/images/Mi foto Julian.jpeg" class="img-fluid rounded shadow-sm mb-2" alt="Foto de perfil" style="max-width: 260px; width: 100%; height: auto;">
       <p class="fw-bold text-secondary mt-2 mb-0">
-        Julián 
+        Julián Jiménez Valdez
       </p>
     </div>
     
     <div class="col-md-8">
       <h3 class="fw-bold mb-3">Sobre mi</h3>
       <p class="text-muted">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quidem alias ipso aperiam esse, unde atque, adiorum vero, adipisci Voluptatibus assumendo?
+        I born in July 4th, 2006 in Cancun, Mexico. I'm a person who live with Spectrum Autist Disorder and ADHD.
       </p>
       <p class="text-muted">
-        Briefly explaining key traits or academic pursuits goes here. Necessitatibus labore cum hic incidunt. porro ducimus quas non!
+        I have abilities to move quickly on the PC's keyboard, often I'm creative in solution problems, and almost always I'm an obsesive person with the details.
       </p>
       <p class="text-muted">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ex aut odio beatae a et vero provident veritatis aperiam sint vel quad  dignissimos officia. Cum cumque tempora voluptatum quo officiis.
+        For Contact me <br>
+        Number phone: 984 185 3344 <br>
+        Email: 2414002@utrivieramaya.edu.mx
       </p>
     </div>
   </div>

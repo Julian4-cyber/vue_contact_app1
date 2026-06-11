b2f1fe441fc46eba5a3f2c5b9769f8e01b4a7004
Julian4-cyber <julian.pherionix@gmail.com>
2026-06-10 23:05:42 -0500

    eleventh commit

--- src/components/gallery.vue
+++ src/components/gallery.vue
@@ -4,30 +4,30 @@
     <div class="row g-4">
       <div class="col-md-4">
         <div class="card h-100 shadow-sm border-0 bg-dark text-white">
-          <img src="../assets/images/" class="card-img-top p-3" alt="Producciones">
+          <img src="../assets/images/Boom P1 frame 1.jpeg" class="card-img-top p-3" alt="Producciones">
           <div class="card-body">
-            <h5 class="card-title fw-bold text-warning">PRODUCCIONES</h5>
-            <p class="card-text small text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis officia, praesentium architecto soluta laborum omnis.</p>
+            <h5 class="card-title fw-bold text-warning">ANIMATION</h5>
+            <p class="card-text small text-light"> My first step in the animation world </p>
           </div>
         </div>
       </div>
       
       <div class="col-md-4">
         <div class="card h-100 shadow-sm border-0 bg-dark text-white">
-          <img src="https://via.placeholder.com/400x300/000000/FFFFFF?text=Bicicleta" class="card-img-top p-3" alt="Valentin Dorantes">
+          <img src="../assets/images/dados1.png" class="card-img-top p-3" alt="Valentin Dorantes">
           <div class="card-body">
-            <h5 class="card-title fw-bold text-danger">Valentin Dorantes</h5>
-            <p class="card-text small text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto beatae perferendis ab maxime ipsa magni.</p>
+            <h5 class="card-title fw-bold text-danger">MODELING</h5>
+            <p class="card-text small text-light"> These are dice modeling in Blender </p>
           </div>
         </div>
       </div>
       
       <div class="col-md-4">
         <div class="card h-100 shadow-sm border-0 bg-dark text-white">
-          <img src="https://via.placeholder.com/400x300/000000/FFFFFF?text=XIB+Creative" class="card-img-top p-3" alt="XIB Creative">
+          <img src="../assets/images/Phase1_Assignment4_JulianJimenez.png" class="card-img-top p-3" alt="XIB Creative">
           <div class="card-body">
-            <h5 class="card-title fw-bold">XIB CREATIVE</h5>
-            <p class="card-text small text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil cupiditate distinctio cum blanditiis numquam.</p>
+            <h5 class="card-title fw-bold"> Images' Editon </h5>
+            <p class="card-text small text-light"> An evidence that I can get a few knowdeledge in images's edition</p>
           </div>
         </div>
       </div>
